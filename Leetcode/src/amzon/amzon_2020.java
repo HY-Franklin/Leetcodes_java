2c1105af452fb33a6aa13437a05679f32cd9f820
HY-Franklin <HY-Franklin@users.noreply.github.com>
2020-01-18 22:27:02 -0500

    1

---
+++ Leetcode/src/amzon/amzon_2020.java
@@ -0,0 +1,22 @@
+package amzon;
+
+import com.sun.javafx.collections.MappingChange;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//Favorite Genres
+class Favorite_Genres{
+    public Map<String, List<String>> favoritegenre(Map<String, List<String>> userMap, Map<String, List<String>> genreMap) {
+
+    }
+}
+
+public class amzon_2020 {
+    public static void main(String[] args) {
+        Map<String,List<String>> userMap=new HashMap<>();
+    }
+
+
+}
